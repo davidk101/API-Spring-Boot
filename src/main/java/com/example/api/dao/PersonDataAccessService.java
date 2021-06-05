774088ac2e044d2333e3db19e1adae6b57a48c19
David Kumar <david.dn.kumar@gmail.com>
2021-06-05 16:20:09 -0500

    update sql queries for documentation purposes

--- src/main/java/com/example/api/dao/PersonDataAccessService.java
+++ src/main/java/com/example/api/dao/PersonDataAccessService.java
@@ -11,9 +11,16 @@ import java.util.UUID;
 // dependence injection
 // DB implementation
 // a container is running and exposing port 5432(5432 is the default port for running a postgres instance using a docker image)
+
+// queries for DB access from terminal for future reference
 // docker exec -it <containerID> bin/bash -> enter container
 // psql -U postgres -> to fire psql queries to postgres
 // \c demodb -> connect to demodb Database
+// \d person -> opens person table
+// CREATE EXTENSION "uuid-ossp";
+// SELECT uuid_generate_v4();
+// INSERT INTO PERSON (id, name) VALUES (uuid_generate_v4(), 'David Kumar');
+
 @Repository("postgres")
 
 public class PersonDataAccessService implements PersonDAO{
